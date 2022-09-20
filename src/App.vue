fcbdc56324f5b4976eda8a5120f4a90f2a99176c
Yaro77 <yari.devel@gmail.com>
2022-09-20 09:43:26 +0300

    Logo BEM fix

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo logo_vite" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="logo logo_vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+  &.logo_vite {
+    &:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
+    }
+  }
+
+  &.logo_vue {
+    &:hover {
+      filter: drop-shadow(0 0 2em #42b883aa);
+    }
+  }
 }
 </style>
